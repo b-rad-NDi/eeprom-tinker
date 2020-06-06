82f1d1d01b6a8a3a4da357c441f5e4c636e7af4a
Brad Love <brad@nextdimension.cc>
2020-06-06 12:12:23 -0500

    Define _GNU_SOURCE
    Signed-off-by: Brad Love <brad@nextdimension.cc>

--- eeprom-tinker.c
+++ eeprom-tinker.c
@@ -12,6 +12,8 @@
  * SPDX-License-Identifier:	GPL-3.0-or-later
 *//////////////////////////////////////////////////////////////////////////////
 
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <limits.h>
 #include <stdbool.h>
